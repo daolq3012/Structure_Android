508d07767767be969a2c923e8f56c5bee4f7a3c5
daolq3012 <dao.le.2511@gmail.com>
2017-03-19 10:57:40 +0700

    Add FragmentScope

---
+++ app/src/main/java/com/fstyle/structure_android/utils/dagger/FragmentScope.java
@@ -0,0 +1,14 @@
+package com.fstyle.structure_android.utils.dagger;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Sun on 3/19/2017.
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FragmentScope {
+}
