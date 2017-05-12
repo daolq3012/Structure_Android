c91186254d44bbcdb6aae3aa8b90fc8955945cec
DaoLQ <le.quang.dao@framgia.com>
2017-05-12 08:08:05 +0700

    MVP catch Exception when call API

--- app/src/main/java/com/fstyle/structure_android/screen/main/MainContract.java
+++ app/src/main/java/com/fstyle/structure_android/screen/main/MainContract.java
@@ -1,6 +1,7 @@
 package com.fstyle.structure_android.screen.main;
 
 import com.fstyle.structure_android.data.model.User;
+import com.fstyle.structure_android.data.source.remote.api.error.BaseException;
 import com.fstyle.structure_android.screen.BasePresenter;
 import com.fstyle.structure_android.screen.BaseView;
 import com.fstyle.structure_android.utils.rx.BaseSchedulerProvider;
@@ -16,7 +17,7 @@ public interface MainContract {
      * View
      */
     interface View extends BaseView {
-        void onSearchError(Throwable throwable);
+        void onSearchError(BaseException throwable);
 
         void onSearchUsersSuccess(List<User> users);
 
