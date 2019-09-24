9b5e04c04bb1d6f0d27fcd26370775b8e23088fd
DaoLQ <duykhanh.t2@gmail.com>
2019-09-24 20:32:50 +0700

    Config auto deploy when merged to master

--- app/src/main/java/com/fstyle/structure_android/MainActivity.kt
+++ app/src/main/java/com/fstyle/structure_android/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fstyle.structure_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 /**
  * Create by DaoLQ
@@ -11,5 +13,6 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    Fabric.with(this, Crashlytics())
   }
 }
