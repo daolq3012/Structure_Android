11840746514c362a98f5e8c843a1ea51f6bcf3c8
DaoLQ <duykhanh.t2@gmail.com>
2019-09-24 15:47:39 +0700

    Convert to Kotlin

---
+++ app/src/main/java/com/fstyle/structure_android/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.fstyle.structure_android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Create by DaoLQ
+ */
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
